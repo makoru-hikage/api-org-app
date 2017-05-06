a350fbb0379484c32112e3cd9dfc651a015883b6
makoru-hikage <kurt.ma.coll@gmail.com>
2017-05-06 14:46:43 +0800

    Add two routes, post route to be tested

--- yoyotest/lib/Yoyotest/Controllers/NotesController.pm
+++ yoyotest/lib/Yoyotest/Controllers/NotesController.pm
@@ -13,7 +13,47 @@ get '/notes' => sub {
 	 	->get_notes
 	 	->get_output_data;
 
+	send_error("No data available", 404) unless $notes->{data}[0];
+
 	send_as JSON => $notes, 
+		{ content_type => 'application/json; charset=UTF-8' };
+};
+
+get '/notes/:id' => sub { 
+	my $username = session('user');
+	send_error("Please login first.", 401) unless $username;
+
+	my $search_filter = from_json(request->body)->{search_filter} if from_json(request->body);
+	$search_filter->{'me.id'} = route_parameters->{id};
+
+	my $notes = Yoyotest::Model::ModelServices::Notes
+		->new($schema)
+	 	->set_search_filter($search_filter)
+	 	->set_user($username)
+	 	->get_notes
+	 	->get_output_data;
+
+	send_error("No data available", 404) unless $notes->{data}[0];
+
+	send_as JSON => $notes->{data}[0], 
+		{ content_type => 'application/json; charset=UTF-8' };
+};
+
+post '/notes' => sub { 
+	my $username = session('user');
+	send_error("Please login first.", 401) unless $username;
+
+	my $input_data = from_json(request->body)->{input_data} if from_json(request->body);
+	$input_data->{'me.id'} = route_parameters->{id};
+
+	my $notes = Yoyotest::Model::ModelServices::Notes
+			->new($schema)
+			->set_input_data($input_data)
+			->set_user($username) #NOTE_1
+			->write_note
+			->get_output_data;
+
+	send_as JSON => $notes->{data}[0], 
 		{ content_type => 'application/json; charset=UTF-8' };
 };
 
