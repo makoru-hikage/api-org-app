dd794da56ec7c793c9d988818def3d566a284271
makoru-hikage <kurt.ma.coll@gmail.com>
2017-05-06 11:58:33 +0800

    Fix errors

--- yoyotest/lib/Yoyotest/Controllers/NotesController.pm
+++ yoyotest/lib/Yoyotest/Controllers/NotesController.pm
@@ -1,20 +1,20 @@
 
 
 get '/notes' => sub { 
-		$check_user->();
-		my $username = session('user');
+	my $username = session('user');
+	send_error("Please login first.", 401) unless $username;
 
-		my $search_filter = from_json(request->body)->{search_filter} if from_json(request->body);
+	my $search_filter = from_json(request->body)->{search_filter} if from_json(request->body);
 
-		my $notes = Yoyotest::Model::ModelServices::Notes
-			->new($schema)
-		 	->set_search_filter($search_filter)
-		 	->set_user($username)
-		 	->get_notes
-		 	->get_output_data;
+	my $notes = Yoyotest::Model::ModelServices::Notes
+		->new($schema)
+	 	->set_search_filter($search_filter)
+	 	->set_user($username)
+	 	->get_notes
+	 	->get_output_data;
 
-		send_as JSON => $notes, 
-			{ content_type => 'application/json; charset=UTF-8' };
+	send_as JSON => $notes, 
+		{ content_type => 'application/json; charset=UTF-8' };
 };
 
 1;
