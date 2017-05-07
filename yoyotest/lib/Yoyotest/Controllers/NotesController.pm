3e082daa1bb7eba4b3c8441de49e0914a685f68a
makoru-hikage <kurt.ma.coll@gmail.com>
2017-05-07 12:39:16 +0800

    POST route now works

--- yoyotest/lib/Yoyotest/Controllers/NotesController.pm
+++ yoyotest/lib/Yoyotest/Controllers/NotesController.pm
@@ -1,4 +1,4 @@
-
+use Yoyotest::Model::Repository;
 
 get '/notes' => sub { 
 	my $username = session('user');
@@ -40,6 +40,48 @@ get '/notes/:id' => sub {
 };
 
 post '/notes' => sub { 
+	my $username = session('user');
+	send_error("Please login first.", 401) unless $username;
+
+	my $input_data = from_json(request->body)->{input_data};
+
+	#Titles must be unique
+	$it_already_exists = Yoyotest::Model::Repository
+		->new($schema, 'Note')
+		->check_uniqueness('title', $input_data->{title});
+
+	send_error("Title already exists", 422) if $it_already_exists;
+
+	my $notes = Yoyotest::Model::ModelServices::Notes
+			->new($schema)
+			->set_input_data($input_data)
+			->set_user($username)
+			->write_note
+			->get_output_data;
+
+	send_as JSON => $input_data, 
+		{ content_type => 'application/json; charset=UTF-8' };
+};
+
+put '/notes/:id' => sub { 
+	my $username = session('user');
+	send_error("Please login first.", 401) unless $username;
+
+	my $input_data = from_json(request->body)->{input_data} if from_json(request->body);
+	$input_data->{'me.id'} = route_parameters->{id};
+
+	my $notes = Yoyotest::Model::ModelServices::Notes
+			->new($schema)
+			->set_input_data($input_data)
+			->set_user($username)
+			->edit_note($id_value)
+			->get_output_data;
+
+	send_as JSON => $notes->{data}[0], 
+		{ content_type => 'application/json; charset=UTF-8' };
+};
+
+del '/notes/:id' => sub { 
 	my $username = session('user');
 	send_error("Please login first.", 401) unless $username;
 
