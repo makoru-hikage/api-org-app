16a2adcee10e101c570d326b5008cefa436ac034
makoru-hikage <kurt.ma.coll@gmail.com>
2017-05-10 14:35:55 +0800

    The directive is somewhat problematic, I've no time. I must decouple it! Damn.

--- yoyotest/lib/Yoyotest/Validators/NoteValidator.pm
+++ yoyotest/lib/Yoyotest/Validators/NoteValidator.pm
@@ -4,30 +4,12 @@ use Validation::Class;
 use Yoyotest::Model::Repository;
 use Yoyotest;
 
-directive 'unique' => ;
-
 field 'title' => {
 	required => 1,
 	max_length => 50,
-	validation => sub {
-
-		my ($self, $proto, $field, $param) = @_;
-
-		if (defined $field->{unique} && defined $param) {
-
-	        my $already_exists = Yoyotest::Model::Repository
-				->new(Yoyotest::schema, 'Note')
-				->check_uniqueness('title', $param);
-
-			my $valid = $already_exists ? 0 : 1;
-
-			return $valid;
-	    }
-	},
 	messages => {
 		required => 'There must be a title!',
 		max_length => 'The number of characters is too much, shorten it',
-		validation => 'Title is already made, do another',
 	} 
 };
 
