06dcd404fa2a35e3b00485fb9c004011518502f5
makoru-hikage <kurt.ma.coll@gmail.com>
2017-05-10 21:10:00 +0800

    Implemented GET PUT POST of notes and todos properly with validation

--- yoyotest/lib/Yoyotest/Controllers/NotesController.pm
+++ yoyotest/lib/Yoyotest/Controllers/NotesController.pm
@@ -26,7 +26,7 @@ get '/notes/:id' => sub {
 	send_error("Please login first.", 401) unless $username;
 
 	my $search_filter = from_json(request->body)->{search_filter} if from_json(request->body);
-	$search_filter->{'me.id'} = route_parameters->{id};
+	$search_filter->{'id'} = route_parameters->{id};
 
 	my $notes = Yoyotest::Model::ModelServices::Notes
 		->new($schema)
@@ -46,9 +46,10 @@ post '/notes' => sub {
 	send_error("Please login first.", 401) unless $username;
 
 	my $request_body = from_json(request->body);
+	$response_body->{input_data}->{username} = $username;
 
 	my $notes = sub {
-		my ($schema, $input_data, $username) = @_;
+		my ($schema, $input_data) = @_;
 
 		Yoyotest::Model::ModelServices::Notes
 			->new($schema)
@@ -62,7 +63,8 @@ post '/notes' => sub {
 		->new($schema)
 		->set_model_service_sub($notes)
 		->set_model_service_data($request_body)
-		->set_validator(Yoyotest::Validators::NoteValidator)	
+		->set_validator(Yoyotest::Validators::NoteValidator)
+		->check_uniqueness('Note','title')	
 		->execute;
 
 	status $response_body->{code};
@@ -71,37 +73,44 @@ post '/notes' => sub {
 	send_as JSON => $response_body;
 };
 
-put '/notes/:id' => sub { 
+put '/notes/:id' => sub {
 	my $username = session('user');
 	send_error("Please login first.", 401) unless $username;
 
-	my $input_data = from_json(request->body)->{input_data};
-	$input_data->{'me.id'} = route_parameters->{id};
+	$id_value = route_parameters->{id};
 
-	my $notes = Yoyotest::Model::ModelServices::Notes
+	my $request_body = from_json(request->body);
+	$response_body->{input_data}->{username} = $username;
+	$response_body->{search_filter}->{id} = $id_value;
+
+	my $notes = sub {
+		my ($schema, $input_data) = @_;
+
+		Yoyotest::Model::ModelServices::Notes
 			->new($schema)
 			->set_input_data($input_data)
 			->set_user($username)
 			->edit_note($id_value)
 			->get_output_data;
+	};
 
-	send_as JSON => $notes, 
-		{ content_type => 'application/json; charset=UTF-8' };
+	my $response_body = Yoyotest::MenuService
+		->new($schema)
+		->set_model_service_sub($notes)
+		->set_model_service_data($request_body)
+		->set_validator(Yoyotest::Validators::NoteValidator)	
+		->check_uniqueness('Note','title')	
+		->execute;
+
+	status $response_body->{code};
+
+	push_response_header content_type => 'application/json; charset=UTF-8';
+	send_as JSON => $response_body;
 };
 
 del '/notes/:id' => sub { 
 	my $username = session('user');
 	send_error("Please login first.", 401) unless $username;
-
-	my $input_data = from_json(request->body)->{input_data} if from_json(request->body);
-	$input_data->{'me.id'} = route_parameters->{id};
-
-	my $notes = Yoyotest::Model::ModelServices::Notes
-			->new($schema)
-			->set_input_data($input_data)
-			->set_user($username) #NOTE_1
-			->write_note
-			->get_output_data;
 
 	send_as JSON => $notes->{data}[0], 
 		{ content_type => 'application/json; charset=UTF-8' };
--- yoyotest/lib/Yoyotest/Controllers/TodosController.pm
+++ yoyotest/lib/Yoyotest/Controllers/TodosController.pm
@@ -1,4 +1,6 @@
-
+use Yoyotest::Model::Repository;
+use Yoyotest::MenuService;
+use Yoyotest::Validators::TodoValidator;
 
 get '/todos' => sub { 
 	my $username = session('user');
@@ -15,6 +17,74 @@ get '/todos' => sub {
 
 	send_as JSON => $todos, 
 		{ content_type => 'application/json; charset=UTF-8' };
+};
+
+post '/todos' => sub { 
+	my $username = session('user');
+	send_error("Please login first.", 401) unless $username;
+
+	my $request_body = from_json(request->body);
+	$response_body->{input_data}->{username} = $username;
+
+	my $notes = sub {
+		my ($schema, $input_data) = @_;
+
+		Yoyotest::Model::ModelServices::Todos
+			->new($schema)
+			->set_input_data($input_data)
+			->set_user($username)
+			->write_note
+			->create_todo
+			->get_output_data;
+	};
+
+	my $response_body = Yoyotest::MenuService
+		->new($schema)
+		->set_model_service_sub($notes)
+		->set_model_service_data($request_body)
+		->set_validator(Yoyotest::Validators::TodoValidator)
+		->check_uniqueness('Note','title')
+		->execute;
+
+	status $response_body->{code};
+
+	push_response_header content_type => 'application/json; charset=UTF-8';
+	send_as JSON => $response_body;
+};
+
+put '/todos/:id' => sub { 
+	my $username = session('user');
+	send_error("Please login first.", 401) unless $username;
+
+	$id_value = route_parameters->{id};
+
+	my $request_body = from_json(request->body);
+	$response_body->{input_data}->{username} = $username;
+	$response_body->{search_filter}->{id} = $id_value;
+
+	my $notes = sub {
+		my ($schema, $input_data) = @_;
+
+		Yoyotest::Model::ModelServices::Todos
+			->new($schema)
+			->set_input_data($input_data)
+			->set_user($username)
+			->edit_todo($id_value)
+			->get_output_data;
+	};
+
+	my $response_body = Yoyotest::MenuService
+		->new($schema)
+		->set_model_service_sub($notes)
+		->set_model_service_data($request_body)
+		->set_validator(Yoyotest::Validators::TodoValidator)
+		->check_uniqueness('Note','title')
+		->execute;
+
+	status $response_body->{code};
+
+	push_response_header content_type => 'application/json; charset=UTF-8';
+	send_as JSON => $response_body;
 };
 
 1;
