595c826dcb844eca2dcdef750530a958ad91645e
makoru-hikage <kurt.ma.coll@gmail.com>
2017-05-19 22:43:02 +0800

    Improve user-related functionalities and allow for custom js and css assets

--- yoyotest/lib/Yoyotest.pm
+++ yoyotest/lib/Yoyotest.pm
@@ -18,14 +18,35 @@ require Yoyotest::Controllers::TodosController;
 
 get '/' => sub {
 
+	my $user = session('user');
+	redirect '/login' unless $user;
+
 	#Load the template
-    template index => { title => 'yoyotest' };
+    template 'index', { 
+    	title => 'Notes', 
+    	page_js => "scripts.js",
+    	page_css => "style.css"
+    };
+
+};
+
+get '/login' => sub {
+
+	redirect '/' if session('user');
+
+	#Load the template
+    template 'login', { 
+    	title => 'Login',
+    	page_js => 'login.js',
+    	page_css => 'login.css' 
+    };
 
 };
 
 #What happens after send_error is invoked.
 hook init_error => sub {
     my $error = shift;
+
     status $error->status;
     
     send_as JSON => { 
@@ -40,8 +61,11 @@ any ['put', 'post'] => '/api/login' => sub {
 	send_as JSON => { message => "Still logged in"} if session('user');
 
 	#Get all the params from HTTP request
-	my $username  = param 'username';
-	my $password  = param 'password';
+	my $username  = from_json(request->body)->{username};
+	my $password  = from_json(request->body)->{password};
+
+	my $there_is_input = $username && $password;
+	send_error("Please fill the username and password", 400) unless $there_is_input;
 
 	#Find and load the user's data from database by username 
 	my $user_model = $schema->resultset('User');
@@ -51,7 +75,6 @@ any ['put', 'post'] => '/api/login' => sub {
 
 	#Hash the input password match against the one in DB
 	my $access_granted = passphrase($password)->matches($password_from_db);
-	
 	send_error("Unauthorized! Wrong username and/or password", 401) unless $access_granted;
 
 	session user => $username_from_db;
@@ -67,6 +90,11 @@ any '/api/logout' => sub {
 	send_as JSON => { message => "You are now logged out."},
 		{ content_type => 'application/json; charset=UTF-8' };
 
+};
+
+any '/logout' => sub {
+	session user => undef;
+	redirect '/login';
 };
 
 true;
